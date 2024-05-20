b5023d7fa274bde1ee3a9b879bf88c0639082617
jonatas <delimajonatas875@gmail.com>
2024-05-20 09:06:24 -0300

    fix: bug de problema de conexão

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
 
 const DB_NAME = "pic-pay";
 const DB_USER = "root";
-const DB_PASS = "";
+const DB_PASS = "password";
 const DB_DRIVE = "mysql";
-const DB_HOST = "192.168.10.59";
+const DB_HOST = "127.0.0.1";
